33802098f28e947a4230c5d468a60580bb960d58
Gabriel Viana <viannadoze@gmail.com>
2023-04-20 21:15:05 -0300

    Typescript on the form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from "phosphor-react"
 import { useForm } from 'react-hook-form'
+import { zodResolver} from '@hookform/resolvers/zod'
+import * as zod from 'zod' 
 
 import { 
     HomeContainer, 
@@ -11,10 +13,27 @@ import {
     MinutesAmountInput 
 } from "./styles";
 
-export function Home() {
-    const { register, handleSubmit, watch } = useForm() 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1,  'Infome a tarefa'),
+    minutesAmount: zod.number()
+    .min(5, 'O ciclo precisa ser de no mínimo de 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo de 60 minutos'),
+})
 
-        function handleCreateNewCycle(data: any) {
+
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    }) 
+
+        function handleCreateNewCycle(data: NewCycleFormData) {
             console.log(data)
         }
 
@@ -49,7 +68,7 @@ export function Home() {
                               placeholder="00"
                               step={5}
                               min={5}
-                              max={60}
+                             
                               {...register('minutesAmount', { valueAsNumber: true})}
                             />
 
